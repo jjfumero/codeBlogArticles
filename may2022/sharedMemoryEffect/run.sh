01aa6c671ef674ac559977cce5404d4cfeb236bd
Juan Fumero <jjfumero@gmail.com>
2022-05-25 08:38:13 +0200

    Script fixed

--- may2022/sharedMemoryEffect/run.sh
+++ may2022/sharedMemoryEffect/run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash 
 
-## Running from 64MB to 8GB per buffer
-## Extended Alloc Memory Mode needed
-#sizes=( 16777216 33554432 67108864 134217728 268435456 536870912 1073741824 2147483648 )
-
-
 ## Running from 64MB to 4GB per buffer
 sizes=( 16777216 33554432 67108864 134217728 268435456 536870912 1073741824 )
 
@@ -26,7 +21,7 @@ done
 for size in ${sizes[@]}
 do
     echo "Running with Size: ${size}"
-    ./levelZeroShared h $size >> HOST.log
+    ./levelZeroShared h $size >> COMBINED.log
 done
 
 #### Run with Host Only Memory
